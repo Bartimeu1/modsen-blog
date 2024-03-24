3ff10fd7f881e918dbd4c50393032308031f60a6
Ivan Sapego <sapego-ivan@list.ru>
2024-03-25 00:26:09 +0300

    feat: added header, banner section

---
+++ src/components/NavMenu/config.ts
@@ -0,0 +1,24 @@
+import { routes } from '@root/constants';
+
+export const navLinks = [
+  {
+    id: 1,
+    name: 'Home',
+    href: routes.home,
+  },
+  {
+    id: 2,
+    name: 'Blog',
+    href: routes.blog,
+  },
+  {
+    id: 3,
+    name: 'About Us',
+    href: routes.about,
+  },
+  {
+    id: 4,
+    name: 'Contact Us',
+    href: routes.contact,
+  },
+];
---
+++ src/constants/routes.tsx
@@ -0,0 +1,9 @@
+export const routes = {
+  home: '/',
+  blog: '/blog',
+  blogPost: '/blog-post',
+  about: '/about',
+  category: '/category',
+  author: '/author',
+  contact: '/contact',
+};
