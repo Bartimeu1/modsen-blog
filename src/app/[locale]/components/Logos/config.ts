c4ed23251803c9309a2ceab0872721cd9d85d961
Ivan Sapego <sapego-ivan@list.ru>
2024-03-27 01:50:33 +0300

    feat: added logos section

---
+++ src/app/[locale]/components/Logos/config.ts
@@ -0,0 +1,13 @@
+import fifthLogo from '@assets/images/fifthLogo.jpg';
+import firstLogo from '@assets/images/firstLogo.jpg';
+import fourthLogo from '@assets/images/fourthLogo.jpg';
+import secondLogo from '@assets/images/secondLogo.jpg';
+import thirdLogo from '@assets/images/thirdLogo.jpg';
+
+export const logosItems = [
+  { id: 1, image: firstLogo },
+  { id: 2, image: secondLogo },
+  { id: 3, image: thirdLogo },
+  { id: 4, image: fourthLogo },
+  { id: 5, image: fifthLogo },
+];
