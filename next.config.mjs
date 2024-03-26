4ce40529dc5182694ce9c2162dcf8a0086198b54
Ivan Sapego <sapego-ivan@list.ru>
2024-03-27 01:42:58 +0300

    feat: added svgr, changed globals colors

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,23 @@
+/** @type {import('next').NextConfig} */
 import createNextIntlPlugin from 'next-intl/plugin';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
 
 const withNextIntl = createNextIntlPlugin();
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const nextConfig = {
+  sassOptions: {
+    includePaths: [join(__dirname, 'styles')],
+    additionalData: '@import "./src/styles/_mixins.scss";',
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+};
 
 export default withNextIntl(nextConfig);
