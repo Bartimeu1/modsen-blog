3d771b5331a54fbcd2caa4883f187ab840691052
Ivan Sapego <sapego-ivan@list.ru>
2024-03-31 14:43:53 +0300

    feat: added custom select

---
+++ src/components/CustomSelect/index.tsx
@@ -0,0 +1,64 @@
+'use client';
+
+import { useRef, useState } from 'react';
+
+import { ChevronIcon } from '@root/constants';
+import { useOnClickOutside } from '@root/hooks';
+import classNames from 'classnames';
+
+import styles from './styles.module.scss';
+import { ICustomSelectProps } from './types';
+export const CustomSelect = (props: ICustomSelectProps) => {
+  const { options } = props;
+
+  const selectRef = useRef(null);
+
+  const [targetOption, setTargetOption] = useState('');
+  const [isSelectActive, setIsSelectActive] = useState(false);
+
+  const onLabelClick = () => {
+    setIsSelectActive((prevState) => !prevState);
+  };
+
+  const closeSelect = () => {
+    setIsSelectActive(false);
+  };
+
+  const onSelectOptionClick = (value: string) => () => {
+    setTargetOption(value);
+    closeSelect();
+  };
+
+  useOnClickOutside(selectRef, closeSelect);
+
+  return (
+    <div className={styles.select} ref={selectRef}>
+      <div
+        className={classNames(styles.label, {
+          [styles.active]: isSelectActive,
+        })}
+        onClick={onLabelClick}
+      >
+        {targetOption ? (
+          <p className={styles.labelValue}>{targetOption}</p>
+        ) : (
+          <p className={styles.placeholder}>Your profession</p>
+        )}
+        <ChevronIcon />
+      </div>
+      {isSelectActive && (
+        <div className={styles.options}>
+          {options.map(({ id, value }) => (
+            <button
+              key={id}
+              onClick={onSelectOptionClick(value)}
+              className={styles.option}
+            >
+              {value}
+            </button>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
---
+++ src/components/CustomSelect/types.ts
@@ -0,0 +1,3 @@
+export interface ICustomSelectProps {
+  options: { id: number; value: string }[];
+}
