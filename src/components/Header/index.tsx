e941e3d9e7efd34c1170b8bdf88809e2375d17d2
Ivan Sapego <sapego-ivan@list.ru>
2024-04-01 01:18:19 +0300

    feat: added video modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,25 @@
+'use client';
+
+import { useState } from 'react';
+
 import { NavMenu } from '@components/NavMenu';
+import { VideoModal } from '@components/VideoModal';
 import { routes } from '@root/constants';
 import Link from 'next/link';
 
 import styles from './styles.module.scss';
 
 export const Header = () => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const onVideoButtonClick = () => {
+    setIsModalVisible((prevState) => !prevState);
+  };
+
+  const closeModal = () => {
+    setIsModalVisible(false);
+  };
+
   return (
     <header className={styles.header}>
       <div className={`container ${styles.headerContainer}`}>
@@ -13,11 +28,16 @@ export const Header = () => {
         </Link>
         <div className={styles.headerControls}>
           <NavMenu />
-          <button className={styles.videoButton} type="button">
+          <button
+            className={styles.videoButton}
+            onClick={onVideoButtonClick}
+            type="button"
+          >
             Video about us
           </button>
         </div>
       </div>
+      {isModalVisible && <VideoModal closeModal={closeModal} />}
     </header>
   );
 };
