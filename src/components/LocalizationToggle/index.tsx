e1aebc744b58d81213d048067d92461099cffec8
Ivan Sapego <sapego-ivan@list.ru>
2024-04-03 14:50:03 +0300

    fix: build and lint fixes

--- src/components/LocalizationToggle/index.tsx
+++ src/components/LocalizationToggle/index.tsx
@@ -14,6 +14,9 @@ export const LocalizationToggle = () => {
 
   const onChangeLocale = () => {
     router.replace(
+      // @ts-expect-error -- TypeScript will validate that only known params
+      // are used in combination with a given pathname. Since the two will
+      // always match for the current route, we can skip runtime checks.
       { pathname, params },
       { locale: locale === 'ru' ? 'en' : 'ru' },
     );
